6aea3addfcedeff55c8821fef863fe9603585c0f
Tomáš Jenej <shamot@neonus.sk>
2016-09-05 10:28:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+require_once 'vendor/autoload.php';
+require_once 'src/functions.php';
+
+$dataDir = __DIR__ . '/data';
+$templateDir = __DIR__ . '/src/templates';
+
+// Load twig
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem($templateDir);
+$twig = new Twig_Environment($loader);
+
+// Load tracks data
+$tracks = loadTracks($dataDir);
+
+
+// Control
+if (isset($_GET['p'])) {
+    $trackName = $_GET['p'];
+    if (isset($tracks[$trackName])) {
+        echo $twig->render('track.html.twig', [
+            'track' => $tracks[$trackName],
+        ]);
+    }
+} else if (!empty($tracks)) {
+    echo $twig->render('tracks.html.twig', [
+        'tracks' => $tracks,
+    ]);
+}
